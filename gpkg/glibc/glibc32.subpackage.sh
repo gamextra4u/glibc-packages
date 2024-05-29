505c6c52581b9590a558484164b4c52822a320a5
Maxython <mixython@gmail.com>
2024-05-30 00:32:02 +0300

    gpkg/glibc: improvements  - adding the glibc32 subpackage  - adding new system calls to the list `fake-syscall.h`  - disabling removal of old locales in `locale-gen`

---
+++ gpkg/glibc/glibc32.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="GNU C Library (32-bit)"
+TERMUX_SUBPKG_DEPENDS="linux-api-headers-glibc"
+TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
+TERMUX_SUBPKG_INCLUDE="
+glibc/lib32/
+glibc/lib/ld-linux-armhf.so.3
+glibc/lib/ld-linux.so.2"
